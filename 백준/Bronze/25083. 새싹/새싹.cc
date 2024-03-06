ab0307b62dd9efc8f7e0644881e03afab707eef9
Sangwoo Cho <155441493+san2u3u@users.noreply.github.com>
2024-03-06 20:32:28 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+int main()
+{
+/*
+			 ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+	  |
+	  |
+*/
+
+	printf("         ,r\'\"7\n");
+	printf("r`-_   ,\'  ,/\n");
+	printf(" \\. \". L_r\'\n");
+	printf("   \`~\\/\n");
+	printf("      |\n");
+	printf("      | ");
+
+	return 0;
+}
